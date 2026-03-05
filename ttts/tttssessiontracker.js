5d2b13494c46a69850281d7a75dc4a7abba84e4e
JOHN DADA <123139569+JonDee2023@users.noreply.github.com>
2026-03-05 11:49:31 +0100

    New js file session tracker added
    JS file to track user login duration.

---
+++ ttts/tttssessiontracker.js
@@ -0,0 +1,69 @@
+let currentSessionId = null;
+
+function loginSession () {
+    let storedSessions = JSON.parse(localStorage.getItem("sessions") || []);
+
+    let newSession = {
+    id: storedSessions.length + 1,
+    email: userEmail,
+    loginTime: new Date().toISOString(),
+    logoutTime: null,
+    duration: null
+    }
+
+    storedSessions.push(newSession)
+
+    localStorage.setItem("sessions", JSON.stringify(storedSessions));
+
+    localStorage.setItem("activeSessionId", newSession.id);
+
+    console.log("Login recorded");
+}
+
+function logoutSession() {
+    let storedSessions = JSON.parse(localStorage.getItem("sessions") || []);
+
+    let activeSessionId = JSON.parse(localStorage.getItem("activesessionId"));
+
+    if (!activeSessionId) {
+        console.log("No active session");
+        return;
+    }
+
+    let ongoingSession = storedSessions.find( s => s.id === activeSessionId);
+
+    if (ongoingSession) {
+        ongoingSession.logoutTime = new Date().toISOString();
+
+    // Calculate duration
+
+    let loginT = new Date(ongoingSession.loginTime);
+
+    let logoutT = new Date(ongoingSession.logoutTime);
+
+    let diffS = logoutT - loginT;
+    let diffM = Math.floor(diffS/60000);
+    ongoingSession.duration = diffM + " minutes"
+
+    localStorage.setItem("sessions", JSON.stringify(storedSessions));
+    
+    localStorage.removeItem("activeSessionId");
+
+    console.log("Logout recorded");
+    }
+
+}
+
+// View all sessions
+console.log(JSON.parse(localStorage.getItem("sessions")));
+
+//Or use a function:
+function showSessions() {
+    let storedSessions = JSON.parse(localStorage.getItem("sessions")) || [];
+    console.table(storedSessions);
+}
+
+// Auto logout when user closes tab:
+window.addEventListener("beforeunload", function () {
+    logoutUser();
+});
